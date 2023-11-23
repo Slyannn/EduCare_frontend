24a46a0e95c9e49464d1f02e0cc8a0cb5992bd05
mbameur <sdongmo@stud.hs-bremen.de>
2023-11-23 11:56:36 +0100

    update ticket

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,29 @@
 
 namespace App\DataFixtures;
 
+
+use App\Entity\Admin;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher  )
     {
-        // $product = new Product();
-        // $manager->persist($product);
+    }
 
+
+    public function load(ObjectManager $manager)
+    {
+        $admin = new Admin();
+        $admin->setEmail('admin@test.fr');
+        $admin->setPassword($this->userPasswordHasher->hashPassword($admin, 'admin'));
+        $admin->setFirstName('Stephane');
+        $admin->setLastName('Admin');
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
         $manager->flush();
     }
 }
